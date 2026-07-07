1f846e598751e66c4a09308e388bc590462df6b5
Luísa Lessa <luisalessaop@gmail.com>
2026-07-07 12:20:58 -0300

    feat: add subject management to student

--- src/studytracker/Main.java
+++ src/studytracker/Main.java
@@ -1,14 +1,25 @@
 package studytracker;
 
+import studytracker.model.Student;
 import studytracker.model.Subject;
 import studytracker.model.StudySession;
 
 public class Main {
     public static void main(String[] args) {
 
+        Student luisa = new Student(
+                "Luísa"
+        );
+
         Subject java = new Subject(
                 "Java",
                 0,
+                "Easy"
+        );
+
+        Subject sql = new Subject(
+                "sql",
+                30,
                 "Easy"
         );
 
@@ -20,7 +31,11 @@ public class Main {
 
         java.addStudyTime(session1.getDurationMinutes());
 
+        luisa.addSubject(java);
+        luisa.addSubject(sql);
+
         System.out.println("Subject: " + java.getName());
         System.out.println("Total minutes studied: " + java.getTotalMinutesStudied());
+        System.out.println("Luísa's subjects: "+ luisa.getSubjects());
     }
 }
--- src/studytracker/model/Student.java
+++ src/studytracker/model/Student.java
@@ -1,10 +1,15 @@
 package studytracker.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
+    private List<Subject> subjects;
 
     public Student(String name) {
         this.name = name;
+        this.subjects = new ArrayList<>();
     }
 
     public void setName(String name) {
@@ -13,6 +18,14 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void addSubject(Subject subject) {
+        this.subjects.add(subject);
     }
 }
 
