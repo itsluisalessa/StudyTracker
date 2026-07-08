36a932f55c41fa8335f15d108ebc186d50e18118
Luísa Lessa <luisalessaop@gmail.com>
2026-07-08 16:43:13 -0300

    feat: validate duplicate subjects before adding

--- src/studytracker/service/StudentService.java
+++ src/studytracker/service/StudentService.java
@@ -6,6 +6,19 @@ import studytracker.model.Subject;
 public class StudentService {
 
     public void addSubject(Student student, Subject subject) {
+
+        String newSubjectName = subject.getName().toUpperCase();
+
+        int subjectSize = student.getSubjects().size();
+
+        for (int i = 0; i < subjectSize; i++) {
+            String nameSubject = student.getSubjects().get(i).getName();
+            String nameSubjectUC = nameSubject.toUpperCase();
+
+            if (newSubjectName.equals(nameSubjectUC)) {
+                return;
+            }
+        }
         student.addSubject(subject);
     }
 }
